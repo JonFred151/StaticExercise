4886103bba02e3e7895acb4fb9805a449b5a0bca
Jon Frederick <jonFred151@gmail.com>
2023-12-22 14:20:11 -0600

    completed execerise

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var celsius = TempConverter.FahrenheitToCelsius(60);
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(20);
+
+            Console.WriteLine($"This is celsius {celsius}, This is {fahrenheit}.");
+        }
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        //Freedom temp to Celsius
+        public static double FahrenheitToCelsius(double fahrenheit)
+        {
+            return (fahrenheit - 32) / 1.8;
+            
+        }
+        //Celsius temp to Freedom temp
+        public static double CelsiusToFahrenheit(double celsius)
+        {
+            return (celsius * 9) / 5 + 32;
+        }
+
+    }
+}
